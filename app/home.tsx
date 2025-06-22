67e4b0b39dcf21a12adbf39ba15314ad42e934fc
sam <3161728@gmail.com>
2025-06-22 11:01:18 +0900

    Install and setup shadcn

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,18 @@
 import type { Route } from "./+types/home";
+import { Button } from "./common/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "KTBC" }, { name: "description", content: "KTBC" }];
 }
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Home</h1>
+      <div className="flex gap-2">
+        <Button variant="outline">Welcome</Button>
+        <Button variant="default">KTBC</Button>
+      </div>
+    </div>
+  );
 }
